b309b261a8161fb65c4498ed1430e6906b38d48b
Andrey Raih <andreyraih@yandex.ru>
2023-11-07 17:03:48 +0800

    Adjust analytic values

--- public/analytic/gtag.js
+++ public/analytic/gtag.js
@@ -1,16 +1,6 @@
 (function () {
-    function getQueryParam(param) {
-        var searchParams = new URLSearchParams(window.location.search);
-        return searchParams.get(param);
-    }
-    var fromValue = getQueryParam('from') || 'direct';
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
-
-    gtag('config', 'G-YRWED3ME0V', {
-        'campaignSource': fromValue,
-        'campaignMedium': 'socials',
-        'campaignName': 'tracking'
-    });
+    gtag('config', 'G-YRWED3ME0V');
 })();
